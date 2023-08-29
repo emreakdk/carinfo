694f704169dbd767511d150cd767ab4c8ccb1fef
emreakdk <emre063819@gmail.com>
2023-08-29 17:28:58 +0300

    markalara eklemeler

--- scripts/markalar.js
+++ scripts/markalar.js
@@ -5,17 +5,20 @@ function fg_load() {
     document.getElementById('loading').style.display = 'none'
 }
 
+let mybutton = document.getElementById("button");
 
-window.onscroll =  (e) => {
-    var scrollTop = window.pageYOffset;
-		console.log(scrollTop);
-		if( scrollTop > 50 ){			
-document.getElementById('button').style.visibility="visible";
-		}
-		else
-		{
-			document.getElementById('button').style.visibility="none";
-		}
-        console.log();
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.bottom = "32px";
+  } else {
+    button.style.bottom = "-100px";
+  }
 }
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
